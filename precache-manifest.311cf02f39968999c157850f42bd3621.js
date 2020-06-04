40b59d779c3beadc8662aac183ba047fe0dda65f
l-limanshu-Singh <himanshusingh1471998@gmail.com>
2020-06-04 09:50:52 +0530

    Updates

---
+++ precache-manifest.311cf02f39968999c157850f42bd3621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d248360b911b27b0b07136d76b97842",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "132305f4e41f7b9149f2",
+    "url": "/To-Do-App-in-React/static/css/main.657d6ae1.chunk.css"
+  },
+  {
+    "revision": "78247785c90c59374fb8",
+    "url": "/To-Do-App-in-React/static/js/2.6b5faf1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-in-React/static/js/2.6b5faf1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132305f4e41f7b9149f2",
+    "url": "/To-Do-App-in-React/static/js/main.cb7e970b.chunk.js"
+  },
+  {
+    "revision": "55384cb1cc546e910d12",
+    "url": "/To-Do-App-in-React/static/js/runtime-main.1b6042bd.js"
+  }
+]);
